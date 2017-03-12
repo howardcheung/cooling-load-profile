4bd8585403cea6e571aa8f3401d0ac13eec603ed
howardcheung <howard.at@gmail.com>
2017-03-12 15:30:02 +0800

    add functions to import data in various format

---
+++ src/data_read.py
@@ -0,0 +1,163 @@
+#!/usr/bin/python3
+
+"""
+    This file contains functions that read data in xlsx format. They also
+    preprocess the data and wrap them into a namedtuple.
+
+    Author: Howard Cheung (howard.at@gmail.com)
+    Date: 2017/12/03
+"""
+
+# import python internal libraries
+from datetime import datetime
+from math import isnan
+import os
+
+# import third party libraries
+import pandas as pd
+
+# import user-defined libraries
+
+
+def read_data(filename: str, header: int=None,
+              time_format: str='%m/%d/%y %I:%M:%S %p CST') -> pd.DataFrame:
+    """
+        This function reads the data in filename that is in specified format
+        and returns a pandas dataframe with time data as the index and
+        'CLG' as the header of the cooling load data. The final dataframe
+        contains 'CLG' as the cooling load data column and 'Duration' as the
+        duration of each data point in seconds.
+
+        Inputs:
+        ==========
+        filename: string
+            path to the data file
+
+        header_col: int, list of ints, default None
+            Row (0-indexed) to use for the column labels of the parsed
+            DataFrame. If a list of integers is passed those row positions
+            will be combined into a MultiIndex. Default None
+
+        time_format: string
+            format of string in time. Default '%m/%d/%y %I:%M:%S %p CST'
+            Please check https://docs.python.org/3.5/library/datetime.html#strftime-and-strptime-behavior
+            for details
+    """
+
+    # initialize the dataframe
+    ext = filename.split('.')[-1]
+
+    # read the file. Read the file as two columns first to conduct
+    # preprocessing before
+    if ext == 'xlsx' or ext == 'xls':
+        with pd.ExcelFile(filename) as xlsx:
+            for sheet_name in xlsx.sheet_names:
+                pddf = pd.read_excel(
+                    xlsx, sheet_name, header=header, names=['Time', 'CLG']
+                )
+                break
+    elif ext == 'csv':
+        pddf = pd.read_csv(filename, header=header, names=['Time', 'CLG'])
+    else:
+        raise ValueError(''.join([
+            'The file extension of the data file cannot be recognized by ',
+            'data_read.read_data(). Exiting.......'
+        ]))
+
+    # make time column as the index
+    pddf.loc[:, 'Time'] = [
+        datetime.strptime(timestr, time_format)
+        for timestr in pddf.loc[:, 'Time']
+    ]
+    pddf.set_index('Time', inplace=True)
+
+    # preprocessing by interpolating invalid columns
+    pddf.loc[:, 'CLG'] = check_nan(pddf['CLG'])
+
+    # calculate the duration of each data point
+
+    return pddf
+
+
+def interpolate_with_s(mid_date: datetime, a_date: datetime, b_date: datetime,
+                       a_value: float, b_value: float) -> float:
+    """
+        Interpolate with datetime.datetime objects bewteen values a and b on
+        two different dates based on their difference in seconds
+
+        Inputs:
+        ==========
+        mid_date: datetime.datetime
+            the date where the value is needed
+
+        a_date: datetime.datetime
+            the date where value a is
+
+        b_date: datetime.datetime
+            the date where value b is
+
+        a_value: float
+            value a
+
+        b_value: float
+            value b
+    """
+
+    return (b_value-a_value)*(mid_date-a_date).seconds /\
+        (b_date-a_date).seconds+a_value
+
+
+def check_nan(wseries: pd.Series) -> pd.Series:
+    """
+        This function checks the values inside the series. If any of them
+        are nan or str, user interpolation with adjacent values to
+        substitute it. Returns the corrected pd.Series
+
+        Inputs:
+        ==========
+        wseries: pd.Series
+            pandas Series data with values in float and index as
+            datetime.datetime object
+    """
+
+    # old slow code
+    for ind, timeind in enumerate(wseries.index):
+        if isinstance(wseries[ind], str) or isnan(wseries[ind]):
+            try:
+                wseries[ind] = interpolate_with_s(
+                    timeind, wseries.index[ind-1], wseries.index[ind+1],
+                    wseries[ind-1], wseries[ind+1]
+                )
+            except IndexError:  # extrapolation
+                try:
+                    wseries[ind] = interpolate_with_s(
+                        timeind, wseries.index[ind-2], wseries.index[ind-1],
+                        wseries[ind-2], wseries[ind-1]
+                    )
+                except IndexError:
+                    # no data available at earlier times are available
+                    wseries[ind] = interpolate_with_s(
+                        timeind, wseries.index[ind+2], wseries.index[ind+1],
+                        wseries[ind+2], wseries[ind+1]
+                    )
+
+    return wseries
+
+
+# testing functions
+if __name__ == '__main__':
+
+    print('Testing ', os.path.basename(__file__))
+    for testfilename in [
+        '../dat/load.csv', '../dat/load.xlsx',
+        '../dat/load_whead.xlsx', '../dat/load_whead.csv'
+    ]:
+        print('Testing file import by using ', testfilename)
+        if 'whead' in testfilename:
+            TEST_DF = read_data(testfilename, header=0)
+        else:
+            TEST_DF = read_data(testfilename, header=None)
+        assert TEST_DF.loc[TEST_DF.index[2], 'CLG'] == 1
+        assert isinstance(TEST_DF.index[0], pd.tslib.Timestamp)
+
+    print('All functions in', os.path.basename(__file__), 'are ok')
