bdb3d09f63911534d25d044f01591b792cc8ae31
howardcheung <howard.at@gmail.com>
2017-03-12 16:44:11 +0800

    add plotting utility functions

---
+++ src/plot_analysis.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+"""
+    This file contains functions that plots the analysis of the buildings.
+
+    Author: Howard Cheung (hcheun@polyu.edu.hk)
+    Date: 2016/01/19
+"""
+
+# import python internal libraries
+from datetime import timedelta
+import itertools
+from math import isnan
+
+# import third party libraries
+import matplotlib
+import pandas as pd
+import pylab as plt
+
+
+# global variables for plotting
+LineStyles = ['-', ':', '-.', '--']*5
+LineWidths = [a for a in itertools.chain.from_iterable(
+    [[1+2*x]*4 for x in range(5)]
+)]
+
+# write functions
+def set_date_for_xaxis():
+    """
+        Function to set the dates on x-axis appropriately
+    """
+    plt.xlabel('Date')
+    locs, labels = plt.xticks()
+    plt.setp(labels, rotation=90)
+
+
+def savefig_for_file(filename: str, diagram_types: list=['pdf'],
+                     dpi: float=300):
+    """
+        Function to save diagram files in .eps, .pdf and .png format.
+        Show the plot and clear the current figure after saving operation.
+
+        Inputs:
+        ==========
+        filename: string
+            path to the file. Note that the folders should be created
+            beforehand
+
+        diagram_types: list
+            types of diagrams to be saved. Default ['pdf']
+
+        dpi: float
+            dpi for diagram. Default 300
+    """
+
+    for ext in diagram_types:
+        plt.savefig(
+            '.'.join([filename, ext]), dpi=300, format=ext, frameon=False
+        )
+    plt.clf()
+
+
+def list_get_legend_handles_labels(list_of_axes: list):
+    """
+        Function to flatten list of handles and legends from AxesSubplot
+        objects
+
+        Inputs:
+        ========
+        list_of_axes: list of matplotlib.axes.Axes
+            list of axes objects in the same plot to be documented
+            by legend
+    """
+
+    handles = []
+    labels = []
+    for axes in list_of_axes:
+        h, l = axes.get_legend_handles_labels()
+        handles += h
+        labels += l
+    return handles, labels
