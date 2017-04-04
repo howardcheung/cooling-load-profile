f782387f24951ef6b6db37f2bde6df5d2b6077e6
howardcheung <howard.at@gmail.com>
2017-04-04 21:42:15 +0800

    Add function to the browse button

--- src/gui_draft.py
+++ src/gui_draft.py
@@ -1,13 +1,14 @@
 ﻿#!/usr/bin/python
-"""
+'''
     This script file contains methods to define the graphical user interface
     of the tool.
 
     Author: Howard Cheung
     email: howard.at@gmail.com
-"""
+'''
 
 # import python internal modules
+from os.path import isfile
 
 # import third party modules
 import wx
@@ -18,12 +19,12 @@ from main_analyzer import main_analyzer
 
 # define global variables
 class MainGUI(wx.Frame):
-    """
+    '''
         Class to hold the object for the main window of the application
-    """
+    '''
 
     def __init__(self, parent, title):
-        """
+        '''
             This is the initilization function for the GUI.
 
             Inputs:
@@ -33,7 +34,7 @@ class MainGUI(wx.Frame):
 
             title: str
                 title of the window
-        """
+        '''
         super(MainGUI, self).__init__(
             parent, title=title, size=(500, 350)
         )  # size of the application window
@@ -43,9 +44,9 @@ class MainGUI(wx.Frame):
         self.Show()
 
     def initui(self):
-        """
+        '''
             Initialize the position of objects in the UI
-        """
+        '''
 
         # define the panel
         panel = wx.Panel(self)
@@ -56,31 +57,31 @@ class MainGUI(wx.Frame):
         # leave space at the top, left and bottom from the text to the
         # other object
         sizer.Add(
-            wx.StaticText(panel, label='BMS data cooling load analyzer'),
+            wx.StaticText(panel, label=u'BMS data cooling load analyzer'),
             pos=(0, 0),  # position: (from top to bottom, from left to right)
             flag=wx.TOP | wx.LEFT | wx.BOTTOM,
             border=10  # border required for flag indicators
         )
 
         # Inputs to the data file path
-        text1 = wx.StaticText(panel, label="Data file path:")
+        text1 = wx.StaticText(panel, label=u'Data file path:')
         sizer.Add(
             text1, pos=(1, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=10
         )
 
         # require additional object for textbox
         # with default path
-        self.tc1 = wx.TextCtrl(panel, value='../dat/load.csv')
+        self.tc1 = wx.TextCtrl(panel, value=u'../dat/load.csv')
         sizer.Add(
             self.tc1, pos=(1, 1), span=(1, 3), flag=wx.TOP | wx.EXPAND,
             border=10
         )
-
-        button1 = wx.Button(panel, label="Browse...")
+        button1 = wx.Button(panel, label=u'Browse...')
+        button1.Bind(wx.EVT_BUTTON, self.OnOpen)
         sizer.Add(button1, pos=(1, 4), flag=wx.TOP | wx.RIGHT, border=10)
 
         # ask for existence of header as a checkbox
-        text2 = wx.StaticText(panel, label='Existence of header:')
+        text2 = wx.StaticText(panel, label=u'Existence of header:')
         sizer.Add(
             text2, pos=(2, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=10
         )
@@ -90,54 +91,54 @@ class MainGUI(wx.Frame):
         sizer.Add(self.cb1, pos=(2, 1), flag=wx.TOP | wx.RIGHT, border=10)
 
         # Inputs to the directory to save the plots
-        text3 = wx.StaticText(panel, label="Directory to save plots:")
+        text3 = wx.StaticText(panel, label=u'Directory to save plots:')
         sizer.Add(
             text3, pos=(3, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=10
         )
 
         # require additional object for textbox
         # with default path
-        self.tc2 = wx.TextCtrl(panel, value='../testplots')
+        self.tc2 = wx.TextCtrl(panel, value=u'../testplots')
         sizer.Add(
             self.tc2, pos=(3, 1), span=(1, 3), flag=wx.TOP | wx.EXPAND,
             border=10
         )
 
-        button2 = wx.Button(panel, label="Browse...")
+        button2 = wx.Button(panel, label=u'Browse...')
         sizer.Add(button2, pos=(3, 4), flag=wx.TOP | wx.RIGHT, border=10)
 
         # Inputs to the format time string
-        text4 = wx.StaticText(panel, label="Format time string:")
+        text4 = wx.StaticText(panel, label=u'Format time string:')
         sizer.Add(
             text4, pos=(4, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=10
         )
 
         # require additional object for textbox
-        self.tc3 = wx.TextCtrl(panel, value='%m/%d/%y %I:%M:%S %p CST')
+        self.tc3 = wx.TextCtrl(panel, value=u'%m/%d/%y %I:%M:%S %p CST')
         sizer.Add(
             self.tc3, pos=(4, 1), span=(1, 3), flag=wx.TOP | wx.EXPAND,
             border=10
         )
 
         # Inputs to the unit of cooling load
-        text5 = wx.StaticText(panel, label="Unit of cooling load:")
+        text5 = wx.StaticText(panel, label=u'Unit of cooling load:')
         sizer.Add(
             text5, pos=(5, 0), flag=wx.TOP | wx.LEFT | wx.BOTTOM, border=10
         )
-        self.tc4 = wx.TextCtrl(panel, value='kW')
+        self.tc4 = wx.TextCtrl(panel, value=u'kW')
         sizer.Add(
             self.tc4, pos=(5, 1), span=(1, 3), flag=wx.TOP | wx.EXPAND,
             border=10
         )
 
         # buttons at the bottom
-        button_ok = wx.Button(panel, label="Analysis")
-        button_ok.Bind(wx.EVT_BUTTON, self.analyzer)
+        button_ok = wx.Button(panel, label=u'Analysis')
+        button_ok.Bind(wx.EVT_BUTTON, self.Analyzer)
         sizer.Add(button_ok, pos=(7, 4))
 
         # button to close the box
-        # button_cancel = wx.Button(panel, label="Cancel")
-        # button_cancel.Bind(wx.EVT_BUTTON, self.onclose)
+        # button_cancel = wx.Button(panel, label=u'Cancel')
+        # button_cancel.Bind(wx.EVT_BUTTON, self.OnClose)
         # sizer.Add(
         #    button_cancel, pos=(7, 4), span=(1, 1),
         #    flag=wx.BOTTOM | wx.RIGHT, border=5
@@ -148,24 +149,49 @@ class MainGUI(wx.Frame):
 
         panel.SetSizer(sizer)
 
-    def showmessage(self):
-        """
+    def ShowMessage(self):
+        '''
             Function to show message about the completion of the analysis
-        """
+        '''
         wx.MessageBox(
-            'Processing Complete', 'Info', wx.OK | wx.ICON_INFORMATION
+            u'Processing Completed', u'Status', wx.OK | wx.ICON_INFORMATION
         )
 
-    def onclose(self, evt):
-        """
+    def OnClose(self, evt):
+        '''
             Function to close the main window
-        """
+        '''
         self.Close(True)
 
-    def analyzer(self, evt):
+    def OnOpen(self, evt):
         """
+            Function to open a file
+            Reference: https://wxpython.org/Phoenix/docs/html/wx.FileDialog.html
+        """
+        #proceed asking to the user the new file to open
+
+        openFileDialog = wx.FileDialog(
+            self, "Open file", "", "",
+            "csv files (*.csv)|*.csv;xls files (*.xls)|*.xls;xlsx files (*.xlsx)|*.xlsx;",
+            wx.FD_OPEN | wx.FD_FILE_MUST_EXIST
+        )
+
+        if openFileDialog.ShowModal() == wx.ID_CANCEL:
+            return     # the user changed idea...
+
+        # proceed loading the file chosen by the user
+        # this can be done with e.g. wxPython input streams:
+        filepath = openFileDialog.GetPath()
+        self.tc1.SetValue(filepath)
+
+        if not isfile(filepath):
+            wx.LogError("Cannot open file '%s'."%openFileDialog.GetPath())
+            return
+
+    def Analyzer(self, evt):
+        '''
             Function to initiate the main analysis.
-        """
+        '''
         # Run the analyzer
         main_analyzer(
             datafilepath=self.tc1.GetValue(),
@@ -176,14 +202,14 @@ class MainGUI(wx.Frame):
         )
 
         # function to be called upon finishing processing
-        wx.CallLater(0, self.showmessage)
+        wx.CallLater(0, self.ShowMessage)
 
 def main():
-    """
+    '''
         Main function to intiate the GUI
-    """
+    '''
     app = wx.App()
-    MainGUI(None, title="BMS data cooling load analzer")
+    MainGUI(None, title=u'BMS data cooling load analzer')
     app.MainLoop()
 
 # run the method for the GUI
